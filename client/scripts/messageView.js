ebb2d5ef107731070fa05f1e27edb4ecbd078a15
Jesse Beard <jjbeard@protonmail.com>
2020-04-04 14:31:34 -0700

    added a better XSS prevention tool

--- client/scripts/messageView.js
+++ client/scripts/messageView.js
@@ -8,9 +8,16 @@ var MessageView = {
     // $('#chats').append('<div class="chat"> </div>');
     // $('.chat').append(`<div class="username" username=${message.username}> ${message.username} <div class='accompmessage' id=${message.objectId}> </div> </div>`).append(`<div class='actualmessages'> ${message.text} </div>`);
     // console.log(message.username + ': ' + message.text);
-    if (!message.text.includes('script') && !message.username.includes('script')) {
-      $('#chats').append(`<div class="username ${message.messageroomname}" "username=${message.username}> ${message.username} <div class='actualmessages'> ${message.text} </div> </div>`);
-    }
+    textToRender = message.text
+    let lt = /</g,
+    gt = />/g,
+    ap = /'/g,
+    ic = /"/g;
+    textToRender = textToRender.toString().replace(lt, "&lt;").replace(gt, "&gt;").replace(ap, "&#39;").replace(ic, "&#34;");
+
+    //if (!message.text.includes('script') && !message.username.includes('script')) {
+      $('#chats').append(`<div class="username ${message.messageroomname}" "username=${message.username}> ${message.username} <div class='actualmessages'> ${textToRender} </div> </div>`);
+    //}
 
   },
 
